abcb06e2ea21c2c9f77caf22cec693a604209148
Kuleshov Alexander <kuleshovmail@gmail.com>
2011-01-14 13:50:15 +0000

    Logger gen_server added

---
+++ src/logger.erl
@@ -0,0 +1,80 @@
+-module(logger).
+
+-author('kuleshovmail@gmail.com').
+-vsn('0.1').
+
+-behavior(gen_server).
+
+-export([init/1]).
+-export([start_link/0, stop/0]).
+-export([handle_call/3, handle_cast/2, handle_info/2]).
+-export([code_change/3]).
+-export([add_log/1]).
+-export([terminate/2]).
+
+%
+% Client API
+%
+start_link() ->
+    LoggerPath = config:get_log_path(config),
+    gen_server:start_link({local, ?MODULE}, ?MODULE, LoggerPath, []).
+
+stop() ->
+    io:format("Logger server stop ~n "),
+    gen_server:cast(?MODULE, stop).
+%
+
+%
+% Log db api
+%
+create_log(LoggerPath) ->
+    case dets:open_file(loggerDisk,[{file,LoggerPath}]) of
+	{ok, Name} ->
+	    Name;
+	{error, Reason} ->
+	    Reason
+    end.
+
+destroy_log() ->
+    dets:close(loggerDisk).
+
+%log_to_txt(DbName) ->
+    
+%
+
+%
+% Log w/r API
+%
+add_log(Text) ->
+    Time = time(),
+    Date = date(),
+    Space = fun () -> " " end,
+    gen_server:call(?MODULE, {add_log,Date, Time, Space, Text}).
+%
+
+%
+% Callback functions
+%
+init(LoggerPath) ->
+    io:format("Logger server start ~n "),
+    create_log(LoggerPath),
+    {ok, null}.
+
+terminate(_Reason, _LoopData) ->
+    destroy_log().
+
+handle_call({add_log, Date, Time, Space, Text }, _From, LoopData) ->
+    Reply = dets:insert(loggerDisk, {Date, Time, Space, Text}),
+    {reply, Reply, LoopData}.
+
+handle_cast(stop, LoopData) ->
+    {stop, normal, LoopData}.
+
+% no useble func
+code_change(_OldVsn, State, _Extra) -> 
+    {ok, State}.
+
+% no useble func
+handle_info(_Msg, LoopData) ->
+    {noreply, LoopData}.
+%
