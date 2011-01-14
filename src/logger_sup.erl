62d796c0e2c4524784210dd0a353cb6209bb4bca
Kuleshov Alexander <kuleshovmail@gmail.com>
2011-01-14 22:26:40 +0000

    Backup dets log storage to txt file added

---
+++ src/logger_sup.erl
@@ -0,0 +1,56 @@
+%%%-------------------------------------------------------------------
+%%% @author  <Kuleshov Alexander>
+%%% @copyright (C) 2011, 
+%%% @doc
+%%%
+%%% @end
+%%% Created : 14 Jan 2011 by  <kuleshovmail@gmail.com>
+%%%-------------------------------------------------------------------
+-module(logger_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0, stop/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+init(_Args) ->
+    RestartStrategy = one_for_one,
+    MaxRestarts = 10,
+    MaxSecondsBetweenRestarts = 3600,
+
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+    Restart = permanent,
+    Shutdown = 2000,
+    Type = worker,
+
+    Child = {logger, {logger, start_link, []},
+	      Restart, Shutdown, Type, [logger]},
+
+    {ok, {SupFlags, [Child]}}.
+
+stop() ->
+    io:format("The logger server shutdown !!! \n"),
+    logger:db_backup(),
+    
+    io:format("The log file backuped \n"),
+    file:delete(config:get_log_path(config)),
+
+    io:format("Temp log storage deleted \n"),
+    exit(whereis(?MODULE), shutdown).
+
