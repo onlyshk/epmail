4e823172f8c27cbea2a9cc7edb12b52b1b102673
Kuleshov Alexander <kuleshovmail@gmail.com>
2011-03-09 17:16:52 +0000

    smtp.hrl added

---
+++ include/smtp.hrl
@@ -0,0 +1,14 @@
+%%%-------------------------------------------------------------------
+%%% @author  <Kuleshov Alexander>
+%%% @copyright (C) 2011, 
+%%% @doc
+%%%
+%%% @end
+%%% Created : 15 Jan 2011 by  <kuleshovmail@gmail.com>
+%%%-------------------------------------------------------------------
+
+-record(state, {
+                socket,
+                client,
+                rcpt 
+               }).
